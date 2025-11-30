d2caad7e70d38f3b8dcf4de197ae94f8ffb4d248
Sanat250405 <sanatnlokeshwar@gmail.com>
2025-11-30 18:59:59 +0530

    Some problem resolved

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,5 @@
 // client/src/api.js
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  timeout: 5000,
-});
-
-export default API;
+const API_URL = 'https://productivity-backend-nqdy.onrender.com/api';
+export default axios.create({ baseURL: API_URL, headers: { 'Content-Type': 'application/json' }});
